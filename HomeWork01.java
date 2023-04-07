f6871c46c22e949c4857f3ac7bb12d5f596a1bf3
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-04-07 23:19:57 +0300

    Task 3

--- HomeWork01.java
+++ HomeWork01.java
@@ -1,9 +1,11 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
-public class HomeWork {
+public class HomeWork01 {
     public static void main(String[] args) {
         // Реализуйте 3 метода, 
         // чтобы в каждом из них получить разные исключения
-        int[] arr = new int[]{4,1,2,4,3,1,10,2};
+        int[] arr = new int[]{8,3,6,4,3,7,10,8};
         int[] arr_2 = new int[]{4,1,2,4,3,1,10,2};
         String[][]arrStr = new String[][]{{"13","21","5","4","1"},{"13","1","4","2","1"},
                                         {"11","43","1","4","16"},{"19","1","4","26","12"}};
@@ -12,6 +14,7 @@ public class HomeWork {
         System.out.println(DivNumbers(arr,num));
         System.out.println(SumNoHundred(arr));
         System.out.println(sum2d(arrStr));
+        System.out.println(Arrays.toString(DivArr(arr, arr_2)));
     }
     public static String PositiveNumbers (int[] arr) {
         for (int i = 0; i < arr.length; i++) if (arr[i] < 0) throw new RuntimeException("Замечено отрицательное число!!");
@@ -47,5 +50,14 @@ public class HomeWork {
             }
         }
         return sum;
+    }
+    public static int [] DivArr (int[] arr, int[] arr_2) {
+        int[] arr_f = new int [arr.length];
+        if (arr.length != arr_2.length) throw new RuntimeException("Длины массивов не равны");
+        for (int i = 0; i < arr.length; i++){
+            if (arr_2[i] == 0) throw new RuntimeException("В делителе замечен ноль!!");
+            arr_f[i] = arr[i]/arr_2[i];
+        }
+            return arr_f;
     }
 }
