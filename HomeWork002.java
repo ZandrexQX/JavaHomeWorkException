1247fb77fa1dec3e0b86df19b504e1310af57650
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-04-11 06:40:10 +0300

    Task 2.2

--- HomeWork002.java
+++ HomeWork002.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class HomeWork002 {
     public static void main(String[] args) {
         FloatInput();
+
+        int [] intArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Task_2_2(intArray, 10, 5);
+
+        
     }
     private static void FloatInput() {
         Scanner sc = new Scanner(System.in);
@@ -12,6 +17,16 @@ public class HomeWork002 {
         } catch (Exception e) {
             System.out.println("Error!");
             FloatInput();
+        }
+    }
+    private static void Task_2_2(int [] intArray, int d, int index) {
+        try {
+            double catchedRes1 = (float)intArray[index] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Catching exception: " + e);
         }
     }
 }
