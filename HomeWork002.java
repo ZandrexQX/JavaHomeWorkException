68ad588a78213864c038d2c92d0ff064dcbdf4c7
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-04-11 06:53:06 +0300

    Task 2.3

--- HomeWork002.java
+++ HomeWork002.java
@@ -7,8 +7,9 @@ public class HomeWork002 {
         int [] intArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         Task_2_2(intArray, 10, 5);
 
-        
+        Task_2_3();
     }
+    
     private static void FloatInput() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input a float: ");
@@ -19,6 +20,7 @@ public class HomeWork002 {
             FloatInput();
         }
     }
+
     private static void Task_2_2(int [] intArray, int d, int index) {
         try {
             double catchedRes1 = (float)intArray[index] / d;
@@ -28,5 +30,29 @@ public class HomeWork002 {
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Catching exception: " + e);
         }
+    }
+
+    public static void printSum(Integer a, Integer b){
+        System.out.println(a + b);
+    }
+
+    public static void Task_2_3(){
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[3] = 9;
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на ноль");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        } 
+     
     }
 }
